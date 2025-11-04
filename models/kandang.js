526c178ecb4921d5144563ee4c132a30db295e35
danny fadli <danyfdly95@gmail.com>
2025-11-04 13:58:58 +0700

    Membuat Module Kandangjs yg berfungsi untuk menentukan databse tersebut

---
+++ models/kandang.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const Kandang = sequelize.define('Kandang', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Nama_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Nama_petugas: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Usia_hewan: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Jenis_hewan: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Tahun_masuk: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }, {
+        tableName: 'komik',
+        timestamps: true
+    });
+    return Komik;
+}  
